db3ab0b28c5e9d0b9ee84bbfd90805f6ef46b1ba
Zahra <47546484+ZahraBaxi@users.noreply.github.com>
2025-10-23 11:50:39 -0700

    madlib

--- madlibs/v2/script.js
+++ madlibs/v2/script.js
@@ -17,12 +17,16 @@
 
         if(!name || !temp || !type){
             alert('please fill in something! we would really love to serve you!');
+            article.innerHTML =`please fill in something! we would really love to serve you!`;
         }
 
         const madlib = 
-        `<p>today it is a<strong>${weather}</strong> afternoon at The Cozy Kettle.</p>
+        `<h2>Welcome to The Cozy Kettle</h2>
+        <p>today it is a<strong>${weather}</strong> afternoon at The Cozy Kettle. There is a soft whisper of chatter in the air, steaming milk in the background with a faint scent of roasted beans and buttery pastries.</p>
         <p>${name} walks in, the bell above the door giving a soft jingle to annouce their entrance. They order a <strong>${temp} ${type}</strong> with a touch of <strong>${flavor}</strong> and a <strong>${pastry}</strong> to go with it.</p>
-        <p>${name} goes to their usual spot, by the window at the front of the cafe and spends the rest of the afternoon <strong>${verb}</strong>. </p>
+        <While waiting, ${name} glances around, a barista hums to the quiet rhythm of jazz in the background and as sunlight filters through the cafe's lace curtains</p>
+        <p>They settle into to their usual spot, by the window at the front of the cafe, watching people pass by as they take their first sip of <strong>${temp} ${type}</strong>. The drink and the pastry feel like a small comfort in an otherwise ordinary day.</p> 
+        <p>The afternoon drifts by as ${name} spends the rest of it <strong>${verb}</strong>, lost in their thoughts, the sound of music echoing through the cafe. The world outside moves slower here, as if time itself pauses to enjoy a cup of tea.</p>
         `;
 
         const article = document.querySelector('article');
